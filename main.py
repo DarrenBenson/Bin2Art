9475c261e2d25191f6b707514ca6f8b0096398e7
Darren Benson <darren.benson@deskpoint.com>
2022-06-09 19:27:35 +0100

    Refactor main image generation

--- main.py
+++ main.py
@@ -12,21 +12,23 @@ def loadFileData(filename):
         with mmap.mmap(file.fileno(), length=0, access=mmap.ACCESS_READ) as fileData:
             return fileData.read()
 
+def generateImage(fileName):
+    outputFileName = getOutputFilename(inputFileName)
+    fileData = loadFileData(fileName)
+    pixelDimention = int(ceil(sqrt(len(fileData) / 3)))
+    outputImage = Image.new("RGBA", (pixelDimention, pixelDimention), "black")
+    pixelCount = 0
+    for x in range(pixelDimention):
+        for y in range(pixelDimention):       
+            rgbValue = [0, 0, 0]
+            for i in range(3):            
+                rgbValue[i] = fileData[pixelCount]
+                if pixelCount < len(fileData) - 1:
+                    pixelCount += 1
+            outputImage.putpixel((x,y),(rgbValue[0], rgbValue[1], rgbValue[2]))
+    resizedImage = outputImage.resize((1920, 1920), Image.Resampling.BOX)
+    resizedImage.save(outputFileName, "PNG")
+
+
 inputFileName = "Chuckie Egg.tap"
-
-outputFileName = getOutputFilename(inputFileName)
-fileData = loadFileData(inputFileName)
-pixelDimention = int(ceil(sqrt(len(fileData) / 3)))
-outputImage = Image.new("RGBA", (pixelDimention, pixelDimention), "black")
-pixelCount = 0
-for x in range(pixelDimention):
-    for y in range(pixelDimention):       
-        rgbValue = [0, 0, 0]
-        for i in range(3):            
-            rgbValue[i] = fileData[pixelCount]
-            if pixelCount < len(fileData) - 1:
-                pixelCount += 1
-        outputImage.putpixel((x,y),(rgbValue[0], rgbValue[1], rgbValue[2]))
-resizedImage = outputImage.resize((1920, 1920), Image.Resampling.BOX)
-resizedImage.save(outputFileName, "PNG")
-
+generateImage(inputFileName)
